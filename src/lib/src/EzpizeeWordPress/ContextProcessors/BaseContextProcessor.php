4ad353b33baa769ae7bead37d3a7d339cf67b3d0
Sothea Nim <sothea.nim@ezpizee.com>
2021-01-06 13:51:01 -0500

    updated readme with new installation instruction

--- src/lib/src/EzpizeeWordPress/ContextProcessors/BaseContextProcessor.php
+++ src/lib/src/EzpizeeWordPress/ContextProcessors/BaseContextProcessor.php
@@ -4,8 +4,6 @@ namespace EzpizeeWordPress\ContextProcessors;
 
 use Ezpizee\ContextProcessor\Base;
 use Ezpizee\MicroservicesClient\Client;
-use Ezpizee\Utils\Response;
-use Psr\Http\Message\ResponseInterface;
 
 abstract class BaseContextProcessor extends Base
 {
@@ -19,16 +17,6 @@ abstract class BaseContextProcessor extends Base
     public function getMicroServiceClient(): Client {return $this->microserviceClient;}
 
     public function isSystemUserOnly(): bool {return false;}
-
-    protected function subRequest(string $method,
-                                  string $path,
-                                  string $query = '',
-                                  array $headers = [],
-                                  array $cookies = [],
-                                  string $bodyContent = '',
-                                  ResponseInterface $response = null): Response {
-        return new Response($method, $path, json_encode(['todo'=>'subRequest method body is empty']));
-    }
 
     protected function isValidAccessToken(): bool {return true;}
 
